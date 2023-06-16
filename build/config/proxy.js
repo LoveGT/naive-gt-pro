d40313793cda78cf1a5b9f4e0a9e58bac6e4ff32
gaotao <gaotao@perfect-zx.com>
2023-06-17 00:48:09 +0800

    feat: 项目配置（一）

---
+++ build/config/proxy.js
@@ -0,0 +1,21 @@
+/**
+ * 设置网络代理
+ * @param isOpenProxy - 是否开启代理
+ * @param envConfig - env环境配置
+ */
+
+export function createViteProxy(isOpenProxy, envConfig) {
+  if (!isOpenProxy) return undefined;
+
+  const proxy = {
+    [envConfig.proxyPattern]: {
+      target: envConfig.target,
+      changeOrigin: true,
+      ws: true,
+      pathRewrite: (path) =>
+        path.replace(new RegExp(`^${envConfig.proxyPattern}`), ""),
+    }
+  };
+  
+  return proxy;
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { getRootPath, getSrcPath } from "./build/utils";
+import { createViteProxy } from "./build/config/proxy";
+import { getServiceEnvConfig } from './.env-config';
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ command, mode }) => {
+  const viteEnv = loadEnv(mode, process.cwd());
+
+  const rootPath = getRootPath();
+  const srcPath = getSrcPath();
+
+  const isOpenProxy = viteEnv.VITE_HTTP_PROXY === 'Y';
+  const envConfig = getServiceEnvConfig(viteEnv);
+
+  return {
+    basePath: viteEnv.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        "~": rootPath,
+        "@": srcPath,
+      },
+    },
+    plugins: [vue()],
+    css: {
+      preprocessorOptions: {
+        //define global scss variable
+        scss: {
+          additionalData: `@use "./src/styles/scss/global.scss" as *;`,
+        },
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+      port: viteEnv.VITE_PORT,
+      open: true,
+      proxy: createViteProxy(isOpenProxy, envConfig),
+    },
+  };
+});
